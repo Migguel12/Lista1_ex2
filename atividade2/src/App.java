07834b471d89d135ec142d5a33c3c3ebfaf19fb1
Migguel12 <128193333+Migguel12@users.noreply.github.com>
2023-06-22 17:46:45 -0300

    Add files via upload

---
+++ atividade2/src/App.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner ler = new Scanner(System.in);
+        int n;
+        System.out.printf("Informe um número: \n");
+        n=ler.nextInt();
+        System.out.println("O número informado foi: " + n);
+
+    
+    
+    }
+}
